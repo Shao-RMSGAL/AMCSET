93777a1e1a9104bcb424ca58217f8f630684ba5a
Autonomous001 <bmejiad@tamu.edu>
2023-10-31 06:50:22 -0500

    Separated the actual inputs to a separate file to reduce the impact on main branch
    I figured that if the inputs were separate to the main.py file, even if they get changed on the input.py then it would not have an impact on the main.py or anyother branches.

--- PythonCode/main.py
+++ PythonCode/main.py
@@ -9,36 +9,12 @@
 ##==================================
 
 from random import random
+from inputs import *
 
 
 ##################INPUT_STARTING_DATA##################
-# Mass of the incident atom
-massp = int(input("Input incident atom mass:") or  "14")
 
-# Charge of incident atom
-# In the original code this variable was called "ZP" but 
-# in this code it would clash with the list that holds the Z-position
-# of the atom being tracked.
-ZProj = int(input("Input incident atom charge Z:") or "14")
-
-# Substrate atom mass
-MASSSUB = int(input("Input substrate atom mass:") or "28")
-
-# Charge of target atom
-ZSUB = int(input("Input substrate atom charge Z:") or "14")
-
-# Substrate density immediately converted to cubic angstrom
-SUBDENSITY = float(input("Input substrate density (atoms/cc):") or "5E22")/ float("1E24")
-
-# Incident atom energy
-INELAB = float(input("Input incident atom energy (keV):") or "50")
-
-# Substrate Window
-SUBWINDOW = int(input("Substrate Window (A):") or "2000")
-
-# Amount of Simulations
-SIMULS = int(input("How many simulations:") or "100")
-
+massp,ZProj,MASSSUB,ZSUB,SUBDENSITY,INELAB,SUBWINDOW,SIMULS = retrieveInputs()
 
 #########################DEFINING_LISTS#########################
 
