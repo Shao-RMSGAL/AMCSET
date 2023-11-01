43338e1d814d95d90c73f4ed7207220edaab8307
Kennethcooper21 <149341317+Kennethcooper21@users.noreply.github.com>
2023-10-31 23:28:40 -0500

    Update #Sub Routine Functions.py

--- PythonCode/#Sub Routine Functions.py
+++ PythonCode/#Sub Routine Functions.py
@@ -24,26 +24,75 @@ def EMagic(ATOMD,X):
 
 ############## TMagic Function ###############
 def TMagic(ATOMD,MASS1, Z1, MASS2, Z2, INELAB, P):
-    PI = math.pi
+    # Constants
+    PI = np.pi
 
+    # Initial parameters
     COLUMBIAVK = 0.0143992 * Z1 * Z2
     MC = MASS1 * MASS2 / (MASS1 + MASS2)
-    INVLAB = math.sqrt(2 * INELAB * 1000 / MASS1)
+    INVLAB = np.sqrt(2 * INELAB / MASS1)
     EC = 0.5 * MC * INVLAB ** 2
-    AU = 0.8854 * 0.529 / (Z1 ** 0.5 + Z2 ** 0.5) ** (2 / 3)
+    AU = 0.8854 * 0.529 / (np.sqrt(Z1) + np.sqrt(Z2)) ** (2/3)
     ELINHARD = EC * AU / COLUMBIAVK
 
-    AA = P ** 2
+    # Calculate RMIN for different energy
+    AA = P**2
     if AA == 0:
         AA = 0.00001
     BB = COLUMBIAVK / EC
     CC = -1
-    COLUMRMIN = 1 / (2 * AA) * (-BB + math.sqrt(BB ** 2 - 4 * AA * CC))
+    COLUMRMIN = 0.5 / AA * (-BB + np.sqrt(BB ** 2 - 4 * AA * CC))
 
     CALTIME = 1
-    RMINTRY1 = COLUMRMIN
-    DV = abs(-DF(RMINTRY1, COLUMBIAVK, Z1, Z2, AU) / EC - 2 * P ** 2 * RMINTRY1)
-    if abs(DV) < 0
+
+    while CALTIME <= 10000:
+        RMINTRY1 = COLUMRMIN
+        DV = abs(-df(RMINTRY1, COLUMBIAVK, Z1, Z2, AU) / EC - 2 * P**2 * RMINTRY1)
+        if abs(DV) < 0.000001:
+            DV = 0.1
+        RMINTRY2 = RMINTRY1 + (1 - f(RMINTRY1, COLUMBIAVK, Z1, Z2, AU) / EC - P**2 * RMINTRY1**2) / DV
+        COLUMRMIN = RMINTRY2
+        CALTIME += 1
+        if abs(RMINTRY2 - RMINTRY1) <= 0.00001:
+            break
+
+    RMIN = (RMINTRY2 + RMINTRY1) / 2
+
+    RBIERSACK = 2 * (EC - f(RMIN, COLUMBIAVK, Z1, Z2, AU)) / RMIN**2 / df(RMIN, COLUMBIAVK, Z1, Z2, AU)
+    BBIERSACK = P / AU
+    ROBIERSACK = 1 / (RMIN * AU)
+    RCBIERSACK = RBIERSACK / AU
+
+    C1BIERSACK = 0.6743
+    C2BIERSACK = 0.009611
+    C3BIERSACK = 0.005175
+    C4BIERSACK = 10.0
+    C5BIERSACK = 6.314
+    ALTHABIERSACK = 1 + C1BIERSACK * ELINHARD**(-0.5)
+    BELTABIERSACK = (C2BIERSACK + ELINHARD**0.5) / (C3BIERSACK + ELINHARD**0.5)
+    GAMABIERSACK = (C4BIERSACK + ELINHARD) / (C5BIERSACK + ELINHARD)
+    ABIERSACK = 2 * ALTHABIERSACK * ELINHARD * BBIERSACK**BELTABIERSACK
+    GBIERSACK = GAMABIERSACK / ((1 + ABIERSACK**2)**0.5 - ABIERSACK)
+    DELTABIERSACK = ABIERSACK * (ROBIERSACK - BBIERSACK) / (1 + GBIERSACK)
+
+    if P == 0:
+        CALPHA1 = PI
+    else:
+        CALPHA1 = 2 * np.arctan(np.sqrt((ROBIERSACK + RCBIERSACK)**2 / (BBIERSACK + RCBIERSACK + DELTABIERSACK)**2 - 1))
+
+    COSPLUSMASS = np.cos(CALPHA1) + MASS1 / MASS2
+    if COSPLUSMASS == 0:
+        THETA1RELATIVE = PI / 2
+    elif COSPLUSMASS < 0:
+        THETA1RELATIVE = PI + np.arctan(np.sin(CALPHA1) / COSPLUSMASS)
+    else:
+        THETA1RELATIVE = np.arctan(np.sin(CALPHA1) / COSPLUSMASS)
+
+    RE = 4 * EC * MC / MASS2 * np.sin(CALPHA1 / 2)**2
+
+    THETA2RELATIVE = (PI - CALPHA1) / 2
+
+    return THETA1RELATIVE, THETA2RELATIVE, RE
 
 ############## AMagic Function ###############
 def AMagic(THETAO, ALPHAO, THETA1RELATIVE, THETA2RELATIVE):
