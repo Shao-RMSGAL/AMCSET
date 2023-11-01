df100429243e7dac128c7a1e0c73631830b463b1
Kennethcooper21 <149341317+Kennethcooper21@users.noreply.github.com>
2023-10-31 22:50:34 -0500

    Update #Sub Routine Functions.py

--- PythonCode/#Sub Routine Functions.py
+++ PythonCode/#Sub Routine Functions.py
@@ -5,9 +5,8 @@ import numpy as np
 import random as random
 
 
-
 ############## EMagic Function ###############
-def EMagic(X,ATOMD):
+def EMagic(ATOMD,X):
     ELOSS = 0
 
     if 1 <= X * 1000 <= 10:
@@ -22,14 +21,99 @@ def EMagic(X,ATOMD):
         ELOSS = ATOMD * (9.28 + 0.00144 * X * 1000) / 1000
     return ELOSS
 
-    
-############## TMagic Function ###############
-def TMagic(MASS1, Z1, MASS2, Z2, INELAB, P):
 
+############## TMagic Function ###############
+def TMagic(ATOMD,MASS1, Z1, MASS2, Z2, INELAB, P):
+    PI = math.pi
+
+    COLUMBIAVK = 0.0143992 * Z1 * Z2
+    MC = MASS1 * MASS2 / (MASS1 + MASS2)
+    INVLAB = math.sqrt(2 * INELAB * 1000 / MASS1)
+    EC = 0.5 * MC * INVLAB ** 2
+    AU = 0.8854 * 0.529 / (Z1 ** 0.5 + Z2 ** 0.5) ** (2 / 3)
+    ELINHARD = EC * AU / COLUMBIAVK
+
+    AA = P ** 2
+    if AA == 0:
+        AA = 0.00001
+    BB = COLUMBIAVK / EC
+    CC = -1
+    COLUMRMIN = 1 / (2 * AA) * (-BB + math.sqrt(BB ** 2 - 4 * AA * CC))
+
+    CALTIME = 1
+    RMINTRY1 = COLUMRMIN
+    DV = abs(-DF(RMINTRY1, COLUMBIAVK, Z1, Z2, AU) / EC - 2 * P ** 2 * RMINTRY1)
+    if abs(DV) < 0
 
 ############## AMagic Function ###############
 def AMagic(THETAO, ALPHAO, THETA1RELATIVE, THETA2RELATIVE):
+    PI = math.pi
+    ALPHA1RELATIVE = random.random() * 2 * PI
+    ALPHA2RELATIVE = PI + ALPHA1RELATIVE
 
+    X1 = math.sin(THETA1RELATIVE) * math.cos(ALPHA1RELATIVE)
+    Y1 = math.sin(THETA1RELATIVE) * math.sin(ALPHA1RELATIVE)
+    Z1 = math.cos(THETA1RELATIVE)
+
+    Y0 = Y1 * math.cos(THETAO) + Z1 * math.sin(THETAO)
+    Z0 = -Y1 * math.sin(THETAO) + Z1 * math.cos(THETAO)
+    X0 = X1
+
+    Z = Z0
+    X = X0 * math.sin(ALPHAO) + Y0 * math.cos(ALPHAO)
+    Y = -X0 * math.cos(ALPHAO) + Y0 * math.sin(ALPHAO)
+
+    if Z > 0:
+        THETA1 = math.atan(math.sqrt(X**2 + Y**2) / Z)
+    elif Z == 0:
+        THETA1 = PI / 2
+    else:
+        THETA1 = PI + math.atan(math.sqrt(X**2 + Y**2) / Z)
+
+    if math.sin(THETA1) != 0:
+        if X > 0:
+            ALPHA1 = math.atan(Y / X)
+        elif X == 0:
+            ALPHA1 = PI - math.copysign(PI / 2, Y)
+        else:
+            ALPHA1 = PI + math.atan(Y / X)
+    else:
+        ALPHA1 = 0
+
+    X1 = math.sin(THETA2RELATIVE) * math.cos(ALPHA2RELATIVE)
+    X2 = math.sin(THETA2RELATIVE) * math.sin(ALPHA2RELATIVE)
+    Z1 = math.cos(THETA2RELATIVE)
+
+    Y0 = Y1 * math.cos(THETAO) + Z1 * math.sin(THETAO)
+    Z0 = -Y1 * math.sin(THETAO) + Z1 * math.cos(THETAO)
+    X0 = X1
+
+    Z = Z0
+    X = X0 * math.sin(ALPHAO) + Y0 * math.cos(ALPHAO)
+    Y = -X0 * math.cos(ALPHAO) + Y0 * math.sin(ALPHAO)
+
+    if Z > 0:
+        THETA2 = math.atan(math.sqrt(X**2 + Y**2) / Z)
+    elif Z == 0:
+        THETA2 = PI / 2
+    else:
+        THETA2 = PI + math.atan(math.sqrt(X**2 + Y**2) / Z)
+
+    if math.sin(THETA2) != 0:
+        if X > 0:
+            ALPHA2 = math.atan(Y / X)
+        elif X == 0:
+            ALPHA2 = PI - math.copysign(PI / 2, Y)
+        else:
+            ALPHA2 = PI + math.atan(Y / X)
+    else:
+        ALPHA2 = 0
+
+    THETA1RELATIVE = THETA1
+    ALPHAO = ALPHA1
+    THETA2RELATIVE = THETA2
+
+    return THETA1RELATIVE, ALPHAO, THETA2RELATIVE, ALPHA2
 
 ############## F Function ###############
 def F (X, COLUMBIAVK, Z1, Z2, AU,Screening):
