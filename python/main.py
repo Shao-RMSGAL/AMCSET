9b16991d99e93af7d94a985ebd5e06daac99fd15
Nathaniel Thomas <nathaniel@swbell.net>
2024-02-09 23:32:46 -0600

    Added scanning code

--- python/main.py
+++ python/main.py
@@ -6,11 +6,14 @@ Last modified:  February 9th, 2024
 Group:          Shao Group, Texas A&M Nuclear Engineering
 
 Description:
-- This is the primary file for Dr. Shao's particle collision simulation software. TODO: Provide a more detailed description
+-   This is the primary file for Dr. Shao's particle collision simulation 
+    software. TODO: Provide a more detailed description
 """
 
 import numpy as np
 import shao_utils as ut
+import os
+import random as rd
 
 # Depth of interest in Angstroms
 # e_range = 80000000
@@ -40,15 +43,17 @@ fly0 = 1000
 fly1 = 1000
 flyC = fly1
 flyJudge = 1
-sizeScan = 100
+
 Resolution = 10
 scandelta = 20
 EMAX = 10000
 
-# Switch value. '0' is for an explicit method, '1' is for the middle-point method, and '2' is for back (implicit) method
+# Switch value. '0' is for an explicit method, '1' is for the middle-point
+# method, and '2' is for back (implicit) method
 switch = 1
 
-# The code has several type declaration statements at this point. Python does not require this.
+# The code has several type declaration statements at this point. Python
+# does not require this.
 
 Ltotal = 0.0
 
@@ -57,55 +62,93 @@ mott = np.zeros((96,5,6))
 
 # TODO: Write code for reading mott potential values in from a text file.
 
-# Matrix for storing screening potentials up to element 92. TODO: Change to read up to element 118
+# Matrix for storing screening potentials up to element 92. TODO: Change to
+# read up to element 118
 scr = np.zeros((92,6))
 
 # TODO: Write code for reading screening potential values
 
 if __name__ == "__main__":
-    # Various output files
-    out_1 = open("./output/PRANGEE40.dat", 'w')
-    out_2 = open("./output/simulEd40.dat", 'w')
-    out_3 = open("./output/simulvimageEd40.dat", 'w')
-    out_4 = open("./output/#DINTLimageEd40.dat", 'w')
-    out_5 = open("./output/eRangeimageEd40.dat", 'w')
-    out_6 = open("./output/ProjectedeRangeimageEd40.dat", 'w')
-    out_7 = open("./output/e-outimageEd40.dat", 'w')
-    out_9 = open("./output/Projected_e_rangeImageEd40.txt", 'w')
-    out_10 = open("./output/20imageEd40.txt", 'w')
-    out_11 = open("./output/crossimageEd40.txt", 'w')
-    out_12 = open("./output/imageEd40.txt", 'w')
-    out_13 = open("./output/VacancyEd40.txt", 'w')
-    out_14 = open("./output/dedx.txt", 'w')
+    current_directory = os.getcwd()
 
-    # iontype determines whether it is an electron bombardment or ion bombardment. The default is 0
-    is_electron = ut.type_check_input("Is this an electron bombardment?", bool, ut.IS_ELECTRON_DEFAULT)
+    if os.path.basename(current_directory) != "python":
+        try:
+            new_directory = os.path.join(current_directory, "python")
+            os.chdir(new_directory)
+        except Exception as e:
+            print("Error: ", e)
+            exit()
+
+
+
+    # Various output files
+    try:
+        current_directory = os.getcwd()
+        output_directory = os.path.join(current_directory, "output")
+        out_1 = open(os.path.join(output_directory, "PRANGEE40.dat"), 'w')
+        out_2 = open(os.path.join(output_directory, "simulEd40.dat"), 'w')
+        out_3 = open(os.path.join(output_directory,
+                                  "simulvimageEd40.dat"), 'w')
+        out_4 = open(os.path.join(output_directory, "DINTLimageEd40.dat"), 'w')
+        out_5 = open(os.path.join(output_directory,
+                                  "eRangeimageEd40.dat"), 'w')
+        out_6 = open(os.path.join(output_directory,
+                                  "ProjectedeRangeimageEd40.dat"), 'w')
+        out_7 = open(os.path.join(output_directory, "e-outimageEd40.dat"), 'w')
+        out_9 = open(os.path.join(output_directory,
+                                  "Projected_e_rangeImageEd40.txt"), 'w')
+        out_10 = open(os.path.join(output_directory,"20imageEd40.txt"), 'w')
+        out_11 = open(os.path.join(output_directory,
+                                   "crossimageEd40.txt"), 'w')
+        out_12 = open(os.path.join(output_directory, "imageEd40.txt"), 'w')
+        out_13 = open(os.path.join(output_directory, "VacancyEd40.txt"), 'w')
+        out_14 = open(os.path.join(output_directory, "dedx.txt"), 'w')
+    except Exception as e:
+        print("Error:", e)
+        ignore_error = ut.type_check_input("Continue despite error?", bool,
+                                           False)
+        if not ignore_error:
+            exit()
+
+    # iontype determines whether it is an electron bombardment or ion
+    # bombardment. The default is 0
+    is_electron = ut.type_check_input("Is this an electron bombardment?", bool,
+                                      ut.IS_ELECTRON_DEFAULT)
 
     if ut.DEBUG:
         print("Is electron:", is_electron)
     # In the case of electron bombardment, ask for energy:
     if is_electron:
         # Ask for the electron input if electron bombardment is being simulated
-        ElecE0 = ut.type_check_input("Input electron energy in keV", float, ut.ELECE0_DEFAULT)
+        ElecE0 = ut.type_check_input("Input electron energy in keV", float,
+                                     ut.ELECE0_DEFAULT)
         if ut.DEBUG:
             print("Energy:", ElecE0)
     else:
         # Ask for ion information if not simulating electron bombardment
-        massp = ut.type_check_input("Input incident atom mass in amu", float, ut.MASSP_DEFAULT)
-        ZP = ut.type_check_input("Input incident charge Z in e", int, ut.ZP_DEFAULT)
-        INELAB = ut.type_check_input("Input incident atom energy in keV", float, ut.INELAB_DEFAULT)
+        massp = ut.type_check_input("Input incident atom mass in amu", float,
+                                    ut.MASSP_DEFAULT)
+        ZP = ut.type_check_input("Input incident charge Z in e", int,
+                                 ut.ZP_DEFAULT)
+        INELAB = ut.type_check_input("Input incident atom energy in keV",
+                                     float, ut.INELAB_DEFAULT)
         if ut.DEBUG:
             print("Mass:", massp)
             print("Charge:", ZP)
             print("Energy:", INELAB)
 
     # Continue on to inputting substrate details
-    MASSSUB = ut.type_check_input("Input substrate mass in amu", float, ut.MASSSUB_DEFAULT)
-    ZSUB = ut.type_check_input("Input substrate atom charge Z in e", int, ut.ZSUB_DEFAULT)
-    SUBDENSITY = ut.type_check_input("Input substrate density in atoms/cc", float, ut.SUBDENSITY_DEFAULT)
+    MASSSUB = ut.type_check_input("Input substrate mass in amu", float,
+                                  ut.MASSSUB_DEFAULT)
+    ZSUB = ut.type_check_input("Input substrate atom charge Z in e", int,
+                               ut.ZSUB_DEFAULT)
+    SUBDENSITY = ut.type_check_input("Input substrate density in atoms/cc",
+                                     float, ut.SUBDENSITY_DEFAULT)
     SUBDENSITY = SUBDENSITY/1E24
-    SUBWINDOW = ut.type_check_input("Input substrate window depth in Angstroms", int, ut.SUBWINDOW_DEFAULT)
-    SIMULS = ut.type_check_input("How many simulations do you want?", int, ut.SIMULS_DEFAULT)
+    SUBWINDOW = ut.type_check_input("Input substrate window depth in Angstroms"
+                                    , int, ut.SUBWINDOW_DEFAULT)
+    SIMULS = ut.type_check_input("How many simulations do you want?", int,
+                                 ut.SIMULS_DEFAULT)
 
     if ut.DEBUG:
         print("Substrate Mass:", MASSSUB)
@@ -114,4 +157,101 @@ if __name__ == "__main__":
         print("Depth:", SUBWINDOW)
         print("Simulations:", SIMULS)
 
-    # More declarations are made in the code at this point, not necessary in python
+    # More declarations are made in the code at this point, not
+    #    necessary in python
+    
+    # Initialize arrays TODO: Change this to allow for arrays of
+    # arbitrary size
+    array_size: int = 2000
+
+    # Represents x position of ion/electron projectiles or target atoms,
+    # up to 2000 in a single bombardment
+    XP = np.zeros(array_size)
+    # Represents y position of ion/electron projectiles or target atoms,
+    # up to 2000 in a single bombardment
+    YP = np.zeros(array_size)
+    # Represents z position of ion/electron projectiles or target atoms,
+    # up to 2000 in a single bombardment
+    ZP = np.zeros(array_size)
+    # Save charge number of `ion/electron projectiles or target atoms
+    CP = np.zeros(array_size, dtype = int)
+    # Save mass of ion/electron projectiles or target atoms
+    mp = np.zeros(array_size, dtype = float)
+    # Save angle with respect to the Z axis
+    THETAP = np.zeros(array_size)
+    # Save angle with respect to the X axis
+    ALPHAP = np.zeros(array_size)
+    # Save energy of ion/electron projectile or target atoms
+    EP = np.zeros(array_size)
+    # Save the particle type. 0 = ion, 1 = electron
+    Ptype = np.zeros(array_size)
+    # Save the profile of ion/electron projectiles along the z axis.
+    ePR = np.zeros(array_size)
+    
+    # TODO: Figure out exactly what THETA2 and ALPHA2 do
+    THETA2 = np.zeros(array_size)
+    ALPHA2 = np.zeros(array_size)
+    
+    # For creating 2D plot of back-scattered electrons
+    Scan = np.zeros((ut.sizeScan + 1, ut.sizeScan + 1, 8))
+    
+    # The QB Code has statements here that zero all the vectors. There
+    # is no need for that thanks to np.zeros()
+    SPUTTER: int = 0
+    """Represents the number of projectiles that sputter"""
+
+    # This seeds the random number generator using the system time.
+    rd.seed(None)
+
+    for SIMUL in range(1, SIMULS):
+        """
+        QB code resets the random seed every 200 iterations. The
+        Mersenne Twister random number generator (used in the python
+        'random' library has a period of 2^199937 - 1, making this
+        step unnecessary)
+        """
+        # Account for the case of an electron
+        if is_electron:
+            # Set electron charge
+            CP[1] = ut.ELECCP
+            # Set electron masss
+            mp[1] = ut.ELECMASSP
+            # Set electron energy
+            EP[1] = ElecE0
+            # The particle type is an electron
+            Ptype[1] = 1
+            # TODO: Figure out what the correctFactor is
+            correctFactor = 1
+        # Account for the case of an ion
+        else:   
+            CP[1] = ZP
+            mp[1] = massp
+            EP[1] = INELAB
+        
+        """
+        ================================================================
+        SCANNING
+        ================================================================
+        """
+
+        for i in range(1, ut.sizeScan):
+            for j in range(1, ut.sizeScan):
+                Scan[i,j,1] = scandelta*(i - ut.sizeScan/2)
+                Scan[i,j,2] = scandelta*(i - ut.sizeScan/2)
+                Scan[i,j,6] = ElecE0
+                # All other entires remain zero
+        
+        """
+        ================================================================
+        END SCANNING
+        ================================================================
+        """
+
+        # The first simulation of a new bombardment event
+        rum = 1
+
+        # Substrate atomic mass
+        MASS2 = MASSSUB
+
+        # Substrate atomic number
+        Z2 = ZSUB
--- python/shao_utils.py
+++ python/shao_utils.py
@@ -6,63 +6,89 @@ Last modified:  February 9th, 2024
 Group:          Shao Group, Texas A&M Nuclear Engineering
 
 Description:
-- This file contains useful constants and functions for use in Dr. Shao's particle collision simulation software
+-   This file contains useful constants and functions for use in Dr. 
+    Shao's particle collision simulation software
 """
 
 from typing import TypeVar, Union
+import scipy as sc
 
 # Constants
 
 DEBUG: bool = True
-"Debug boolean. If True, then enable debug mode. Otherwise, disable debug mode."
+"""Debug boolean. If True, then enable debug mode. Otherwise, disable
+debug mode.
+"""
+
+DOSE: float = 0
+"""Represents dose"""
 
 ELECCP: int = -1
-"This is the charge of an electron in elementary charge units"
+"""This is the charge of an electron in elementary charge units"""
 
-ELECMASSP: float = 5.45E-4
-"This is the mass of an electron in amu."
+ELECMASSP: float = sc.constants.electron_mass/sc.constants.atomic_mass
+"""This is the mass of an electron in amu."""
 
+# TODO: See what this value represents, and if there is a more robust way of
+# implementing it.
 EPRBIN: float = 1.5E6
-"This is the depth profile interval for an electron in Angstroms."
+"""This is the depth profile interval for an electron in Angstroms."""
+
+sizeScan = 100
+"""Represents the size of a scan used in electron back-scattering plot"""
 
 WINDOWX: int = 630
-"Screen pixel width"
+"""Screen pixel width"""
 
 WINDOWY: int = 330
-"Screen pixel height"
+"""Screen pixel height"""
 
 
 # Input default values
 
 ELECE0_DEFAULT: float = float(10000)
-"This is the default value for electron energy in keV."
+"""This is the default value for electron energy in keV."""
 
 INELAB_DEFAULT: float = float(50)
-"This is the default ion incident energy in keV."
+"""This is the default ion incident energy in keV."""
 
 IS_ELECTRON_DEFAULT: bool = False
-"This represents the default value for ion type. False means that the ion is not an electron, while True means that the ion is an electron."
+"""This represents the default value for ion type. False means that the
+ion isnot an electron, while True means that the ion is an electron.
+"""
 
 MASSP_DEFAULT: float = 27.9
-"This is the default value for ion mass in amu. Default mass is that of a silicon ion."
+"""This is the default value for ion mass in amu. Default mass is that
+of a silicon ion.
+"""
 
 MASSSUB_DEFAULT: float = 55.85
-"This is the default value for substrate mass in amu. Default mass is that of an iron atom."
+"""This is the default value for substrate mass in amu. Default mass is
+that of an iron atom.
+"""
 
 SIMULS_DEFAULT: int = 1000
-"This is the default number of simulations to carry out. Each simulation is that of a single particle."
+"""This is the default number of simulations to carry out. Each
+simulation is that of a single particle.
+"""
 
 SUBDENSITY_DEFAULT: float = 8.382E22
-"This is the default value for the density of the substrate in atoms/cubic centimeters. Default density is that of iron."
+"""This is the default value for the density of the substrate in
+atoms/cubic centimeters. Default density is that of iron.
+"""
 
 SUBWINDOW_DEFAULT: int = 80000000
-"This is the default value for the depth of the window in Angstroms."
+"""This is the default value for the depth of the window in Angstroms."""
 
 ZP_DEFAULT: int = 14
-"This is the default charge number of the incident atom. Default charge is that of silicon."
+"""This is the default charge number of the incident atom. Default
+charge is that of silicon.
+"""
 
 ZSUB_DEFAULT: int = 26
-"This is the default charge number of the substrate atom. Default charge is that of iron."
+"""This is the default charge number of the substrate atom. Default
+charge is that of iron.
+"""
 
 # Functions
 
@@ -84,7 +110,8 @@ def EMAGIC(X):
 def F(X, COLUMBIAVK, Z1, Z2, AU):
     return -1
 
-def IMAGE(Scan, NewDeltaZ, NewDeltaX, NewDeltaY, NewTheta, NewAlpha, NewEP, Resolution, size):
+def IMAGE(Scan, NewDeltaZ, NewDeltaX, NewDeltaY, NewTheta, NewAlpha, NewEP,
+          Resolution, size):
     return -1
 
 def IoniElecLoss(ZSUB, DENSITY, EleE):
@@ -98,7 +125,8 @@ T = TypeVar('T')
 
 def type_check_input(prompt: str, type: Union[type, T], default: T) -> T:
     """
-    Performs type-checking for simple inputs. Inputs can either be a boolean, a integer, or float
+    Performs type-checking for simple inputs. Inputs can either be a
+    boolean, a integer, or float
 
     Args:
         prompt (str):   Prompt shown to the user
@@ -108,53 +136,53 @@ def type_check_input(prompt: str, type: Union[type, T], default: T) -> T:
     if not isinstance(default, type):
             raise ValueError("Default type does not match specified type")
     if type is bool:
-        while True:
-            try:
-                prompt = prompt + " (y/n - default = " + (" y" if default else "n") + "): "
-                output = input(prompt)
-                if output == 'y':
-                    output = True
-                    break
-                elif output == 'n':
-                    output = False
-                    break
-                elif output == "":
-                    output = default
-                    break
-                else:
-                    raise ValueError("Invalid option: " + output)
-            except ValueError as e:
-                print("Error:", e)
-                print("Please enter either y or n")
+        try:
+            mod_prompt = prompt + " (y/n - default = " + (" y" if default else
+                                                          "n") + "): "
+            output = input(mod_prompt)
+            if output == 'y':
+                output = True
+            elif output == 'n':
+                output = False
+            elif output == "":
+                output = default
+            else:
+                raise ValueError("Invalid option: " + output)
+        except ValueError as e:
+            print("Error:", e)
+            print("Please enter either y or n")
+            return type_check_input(prompt, type, default)
     elif type is int:
-        while True:
-            try:
-                prompt = prompt + " (integer, default = " + str(default) + "): "
-                output = input(prompt)
-                if output == "":
-                    output = default
-                    break
-                else:
-                    output = int(output)
-                    break
-            except ValueError as e:
-                print("Error:", e)
-                print("Please enter an integer")
+        try:
+            mod_prompt = prompt + " (integer, default = " + str(default)+ "): "
+            output = input(mod_prompt)
+            if output == "":
+                output = default
+            else:
+                output = int(output)
+        except ValueError as e:
+            print("Error:", e)
+            print("Please enter an integer")
+            return type_check_input(prompt, type, default)
     elif type is float:
-        while True:
-            try:
-                prompt = prompt + " (floating point number, default = " + str(default) + "): "
-                output = input(prompt)
-                if output == "":
-                    output = default
-                    break
-                else:
-                    output = float(output)
-                    break
-                    
-            except ValueError as e:
-                print("Error:", e)
-                print("Please enter a floating-point number")
+        try:
+            mod_prompt = prompt + " (floating point number, default = " + \
+                        str(default) + "): "
+            output = input(mod_prompt)
+            if output == "":
+                output = default
+            else:
+                output = float(output)            
+        except ValueError as e:
+            print("Error:", e)
+            print("Please enter a floating-point number")
+            return type_check_input(prompt, type, default)
     else:
         raise ValueError("Invalid type")
     return output
+
+if __name__ == "__main__":
+    """Only run this module as main for debugging purposes"""
+
+    print("NOT THE MAIN FUNCTION, PLEASE RUN main.py!")
+    print("Electron mass: ", ELECMASSP)
