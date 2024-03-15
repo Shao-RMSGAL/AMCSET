4941f35ce1bfaca1bb5f6f1a01a8f514a2859d59
Nathaniel Thomas <nathaniel@swbell.net>
2024-03-15 12:53:35 -0500

    Added main header file for testing

---
+++ include/eSRIM.h
@@ -0,0 +1,24 @@
+/******************************************************************************
+ * Filename:        main.h
+ * Project:         eSRIM
+ * Author:          Nathaniel Thomas
+ * Date Created:    March 15, 2024
+ * Date Modified:   March 15, 2024
+ * File Version:    1.1
+ * Group:           Dr. Shao's RMSLCF Group
+ *
+ * Description:
+ * This is the main header file for the eSRIM code developed by Dr. Shao and converted
+ * to the C++ programming language by Nathaniel Thomaas
+ *
+ * Revision History
+ * 1.0:
+ * - File created and initial program structure made
+ *****************************************************************************/
+#ifndef MAIN_H
+#define MAIN_H
+
+// Main program  start
+int main(int argc, char* argv[]) {}
+
+#endif
